c24519c96f2bcc1d8353151a7484b359e91b181b
Smileycorp <callum.j.longbottom@gmail.com>
2023-07-21 23:29:43 +0100

    finish adding gen 3 abilities

---
+++ src/test/net/smileycorp/battlesimulator/EngineTest.java
@@ -0,0 +1,11 @@
+
+public class EngineTest {
+
+    public static void main(String[] args) throws Exception {
+
+        String path = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+        File file = new File(path + "kyogre.json");
+        Pokemon
+    }
+
+}
